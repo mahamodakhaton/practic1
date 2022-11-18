1da7aadd2011caf9e239e2678b506ffc3a77619f
mahamoda khaton <mhmdkhton@gmail.com>
2022-11-18 01:06:06 -0800

    hw

---
+++ src/package1/Class1.java
@@ -0,0 +1,17 @@
+package package1;
+/*Project name start with upper case
+ * package name start with lower case
+ * class name start with upper case
+ * 
+ */
+public class Class1 {
+	// Main method is the entry point of java
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("My name is MahamodaKhaton");
+	System.out.println("My address is 86-45 st james Ave Elmhurst,Newyork,");
+	}
+
+}
